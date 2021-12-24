ea714bb40b9dc68ba3fb6479cd1ddc16ba89dfb9
Lorenzo Busoni <lbusoni@gmail.com>
2021-12-24 23:58:20 +0100

    fix typos

--- plico_motor_server/devices/abstract_motor.py
+++ plico_motor_server/devices/abstract_motor.py
@@ -1,6 +1,5 @@
 import abc
 from six import with_metaclass
-from plico.utils.decorator import returns
 
 
 class AbstractMotor(with_metaclass(abc.ABCMeta, object)):
@@ -14,37 +13,66 @@ class AbstractMotor(with_metaclass(abc.ABCMeta, object)):
 
     @abc.abstractmethod
     def position(self):
+        '''
+        Returns
+        ------
+        position: int
+            axis position in steps
+        '''
         assert False
 
     @abc.abstractmethod
     def steps_per_SI_unit(self):
-        assert false
+        '''
+        Returns
+        ------
+        steps_per_SI_unit: float
+            steps/m if linear motor or steps/rad if rotative motor
+        '''
+        assert False
 
     @abc.abstractmethod
     def was_homed(self):
-        assert false
+        assert False
 
     @abc.abstractmethod
     def type(self):
-        assert false
+        assert False
 
     @abc.abstractmethod
     def is_moving(self):
-        assert false
+        assert False
 
     @abc.abstractmethod
     def last_commanded_position(self):
-        assert false
+        '''
+        Returns
+        ------
+        last commanded position: int
+            last set point in steps
+        '''
+        assert False
 
     # --------------
     # Commands
 
     @abc.abstractmethod
     def home(self):
+        '''
+        Perform homing / initialization procedure
+        '''
         assert False
 
     @abc.abstractmethod
     def move_to(self):
+        '''
+        Move to an absolute position
+
+        Parameters
+        ----------
+        position: int
+            desired position in steps
+        '''
         assert False
 
     @abc.abstractmethod
@@ -54,5 +82,4 @@ class AbstractMotor(with_metaclass(abc.ABCMeta, object)):
     @abc.abstractmethod
     def deinitialize(self):
         assert False
-
 
