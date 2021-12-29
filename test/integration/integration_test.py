d7cfd31ded252a97133eaa239fcaa9293587169f
Alfio Puglisi <alfio.puglisi@inaf.it>
2021-12-29 13:02:30 +0100

    Commented out tests that use the real Picomotor

--- test/integration/integration_test.py
+++ test/integration/integration_test.py
@@ -117,11 +117,11 @@ class IntegrationTest(unittest.TestCase):
             '%s.log' % Constants.SERVER_1_CONFIG_SECTION)
         Poller(5).check(MessageInFileProbe(
             Runner.RUNNING_MESSAGE, controllerLogFile))
-        controller2LogFile = os.path.join(
-            self.LOG_DIR,
-            '%s.log' % Constants.SERVER_2_CONFIG_SECTION)
-        Poller(5).check(MessageInFileProbe(
-            Runner.RUNNING_MESSAGE, controller2LogFile))
+#        controller2LogFile = os.path.join(
+#            self.LOG_DIR,
+#            '%s.log' % Constants.SERVER_2_CONFIG_SECTION)
+#        Poller(5).check(MessageInFileProbe(
+#            Runner.RUNNING_MESSAGE, controller2LogFile))
 
     def _buildClients(self):
         ports1 = ZmqPorts.fromConfiguration(
@@ -169,25 +169,25 @@ class IntegrationTest(unittest.TestCase):
         self.assertEqual(self.client1.position(), 0)
 
     def _test_picomotor_raise_exception_on_home(self):
-        self.assertRaises(PicomotorException, self.client2.home)
+        self.assertRaises(PicomotorException, self.client1.home)
 
     def _test_move_to(self):
         self.client1.move_to(123)
-        self.client2.move_to(-34)
+#        self.client2.move_to(-34)
         Poller(3).check(ExecutionProbe(
             lambda: self.assertEqual(123,
                                      self.client1.position())))
-        Poller(3).check(ExecutionProbe(
-            lambda: self.assertEqual(int(-34),
-                                     self.client2.position())))
+#        Poller(3).check(ExecutionProbe(
+#            lambda: self.assertEqual(int(-34),
+#                                     self.client2.position())))
 
     def _test_move_by(self):
         self.client1.move_by(-23)
-        self.client2.move_by(10)
+#        self.client2.move_by(10)
         Poller(3).check(ExecutionProbe(
             lambda: self.assertEqual(100, self.client1.position())))
-        Poller(3).check(ExecutionProbe(
-            lambda: self.assertEqual(-24, self.client2.position())))
+#        Poller(3).check(ExecutionProbe(
+#            lambda: self.assertEqual(-24, self.client2.position())))
 
     def test_main(self):
         self._buildClients()
@@ -197,10 +197,10 @@ class IntegrationTest(unittest.TestCase):
         self._testProcessesActuallyStarted()
         self._test_at_boot_is_not_homed()
         self._test_home_and_get_position()
-        self._test_picomotor_raise_exception_on_home()
+#        self._test_picomotor_raise_exception_on_home()
         self._test_move_to()
         self._test_move_by()
-        self._test_get_snapshot()
+#        self._test_get_snapshot()
         self._test_server_info()
         self._check_backdoor()
         self._wasSuccessful = True
